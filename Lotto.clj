1ab4ee1a3dd1a6c0ee8d3fcdff8b47ce7cccc087
Piotr Gajowniczek <piotrgajow@gmail.com>
2016-05-19 00:21:01 +0200

    Add lotto calculations

--- Lotto.clj
+++ Lotto.clj
@@ -1,21 +1,56 @@
-
-(defn testLottery []
-    (println "Test Lottery")
+( defn total [ price & winnings ] 
+    ( - ( reduce + winnings ) price )
 )
 
-(defn menu []
-    (do 
-        (println "0. Exit")
-        (println "1. Test")
-        (let [in (read-line)] 
-            (case in
-                "0" (System/exit 0)
-                "1" (testLottery)
-                (menu)
+( defn factorial [ x ]
+    ( reduce *' ( range 1 ( inc x ) ) )
+)
+
+( defn combinations [ x y ]
+    ( / ( factorial y ) ( * ( factorial x ) ( factorial ( - y x ) ) ) )
+)
+
+( defn chances [ hit winning total ]
+    ( let [ hitComb ( combinations hit winning )
+            missComb ( combinations ( - winning hit ) ( - total winning ) )
+            totalComb ( combinations winning total )
+        ] 
+        ( / ( * hitComb missComb ) totalComb )
+    )
+)
+
+( defn chancesPrized [ hit winning total prize ] 
+    ( * ( chances hit winning total ) prize )
+)
+
+( defn lotto [] 
+    ( println "Lotto" )
+    ( println "Cumulation?" )
+    ( let [ cumulation ( Integer/parseInt ( read-line ) ) 
+            prize6 ( chancesPrized 6 6 49 cumulation )
+            prize5 ( chancesPrized 5 6 49 5300 )
+            prize4 ( chancesPrized 4 6 49 170 )
+            prize3 ( chancesPrized 3 6 49 24 )
+        ] 
+        ( println "Result" ( double ( total 3 prize6 prize5 prize4 prize3 ) ) )
+    )
+)
+
+( defn menu []
+    ( do 
+        ( println "" )
+        ( println "0. Exit" )
+        ( println "1. Lotto" )
+        ( println "Choice?" )
+        ( let [ in ( read-line ) ] 
+            ( case in
+                "0" ( System/exit 0 )
+                "1" ( lotto )
+                ()
             )
+            ( menu )
         )
     )
 )
 
-(menu)
-
+( menu )
