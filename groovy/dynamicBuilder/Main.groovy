073a894fb7fbef689ba55ddd2a31c1010ddc59c8
Piotr Gajowniczek <piotrgajow@gmail.com>
2017-05-24 13:13:33 +0200

    Change user password

--- groovy/dynamicBuilder/Main.groovy
+++ groovy/dynamicBuilder/Main.groovy
@@ -44,5 +44,5 @@ class Builder {
 }
  
 def builder = new Builder()
-println builder.forClass(User).withName('Jan').withSurname('Kowalski').withLogin('jankow').withEmail('jan.kowalski@gmail.com').withPassword('FASDfadf#$%wtrw').build()
+println builder.forClass(User).withName('Jan').withSurname('Kowalski').withLogin('jankow').withEmail('jan.kowalski@gmail.com').withPassword('kow123').build()
 println builder.forClass(User).withLogin('jan').build()
