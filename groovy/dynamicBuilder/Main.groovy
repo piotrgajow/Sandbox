246b7adc5737e04d423fb83ded463f6f098401f0
Piotr Gajowniczek <piotrgajow@gmail.com>
2017-05-24 13:19:01 +0200

    Extract User class to separate source file

--- groovy/dynamicBuilder/Main.groovy
+++ groovy/dynamicBuilder/Main.groovy
@@ -1,25 +1,3 @@
-class User {
-	def login
-	def name
-	def surname
-	def email
-	def password
-
-	void setPassword(String password) {
-		this.password = hashPassword(password)
-	}
-
-	String hashPassword(String password) {
-		def digest = java.security.MessageDigest.getInstance("SHA-256")
-		digest.update( password.bytes )
-		return new BigInteger(1,digest.digest()).toString(16).padLeft(32, '0')
-	}
- 
-	String toString() {
-		return "$name $surname ($login) - $email, $password"
-	}
-}
- 
 class Builder {
 	def inst
  
