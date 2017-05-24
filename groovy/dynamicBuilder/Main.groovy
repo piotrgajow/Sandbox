ad2352f2f558577fcc093a4c9b89ba6eab5c0abe
Piotr Gajowniczek <piotrgajow@gmail.com>
2017-05-24 13:48:51 +0200

    Add assert to Main

--- groovy/dynamicBuilder/Main.groovy
+++ groovy/dynamicBuilder/Main.groovy
@@ -1,3 +1,6 @@
 def builder = new Builder()
+
 println builder.forClass(User).withName('Jan').withSurname('Kowalski').withLogin('jankow').withEmail('jan.kowalski@gmail.com').withPassword('kow123').build()
-println builder.forClass(User).withLogin('jan').build()
+def user = builder.forClass(User).withLogin('jan').build()
+println user
+assert user.toString() == 'null null (jan) - null, null'
