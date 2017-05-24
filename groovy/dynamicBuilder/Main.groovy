6fff046a390ca4afcef71df116d9639071820cb8
Piotr Gajowniczek <piotrgajow@gmail.com>
2017-05-24 13:10:00 +0200

    Add password hashing

--- groovy/dynamicBuilder/Main.groovy
+++ groovy/dynamicBuilder/Main.groovy
@@ -4,6 +4,16 @@ class User {
 	def surname
 	def email
 	def password
+
+	void setPassword(String password) {
+		this.password = hashPassword(password)
+	}
+
+	String hashPassword(String password) {
+		def digest = java.security.MessageDigest.getInstance("SHA-256")
+		digest.update( password.bytes )
+		return new BigInteger(1,digest.digest()).toString(16).padLeft(32, '0')
+	}
  
 	String toString() {
 		return "$name $surname ($login) - $email, $password"
