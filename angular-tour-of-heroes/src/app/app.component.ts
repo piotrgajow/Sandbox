f79ea027d816c487cb7f6d9b4931a8438a048b74
Piotr Gajowniczek <piotrgajow@gmail.com>
2017-11-19 13:29:31 +0100

    Remove angular related files

--- angular-tour-of-heroes/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    templateUrl: './app.component.html',
-    selector: 'app-root',
-})
-export class AppComponent {
-    title = 'Tour of Heroes';
-}
